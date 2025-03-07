3002e7a38317bf3e3047b6f1cbd92d8504fdc9d7
dahniardd <dahniardvn09@gmail.com>
2025-03-07 15:18:11 +0700

    Update Retrieving Single Models

--- MInggu4/Jobsheet4/app/Http/Controllers/UserController.php
+++ MInggu4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -10,18 +10,12 @@ class UserController extends Controller
 {
     public function index()
     {
-       // tambah data user dengan Eloquent Model
-    $data = [
-        'level_id' => '2',
-        'username' => 'manager_tiga',
-        'nama' => 'Manager 3',
-        'password' => Hash::make('123456'),
-    ];
+        //$user = UserModel::firstwhere('level_id', 1)->first(); 
+        //return view('user', ['data' => $user]);
 
-    UserModel::insert($data); // tambahkan data ke tabel m_user
- 
-    // coba akses model UserModel
-    $user = UserModel::all(); // ambil semua data dari tabel m_user
-    return view('user', ['data' => $user]);
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);    
     }
 }
