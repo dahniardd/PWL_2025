8569df5d952b1b5be6ca026045ab5b990dc7579e
dahniardd <dahniardvn09@gmail.com>
2025-03-10 09:00:36 +0700

    add $fillable

--- MInggu4/Jobsheet4/app/Http/Controllers/UserController.php
+++ MInggu4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -10,12 +10,18 @@ class UserController extends Controller
 {
     public function index()
     {
-        //$user = UserModel::firstwhere('level_id', 1)->first(); 
-        //return view('user', ['data' => $user]);
+       // tambah data user dengan Eloquent Model
+    $data = [
+        'level_id' => 2,
+        'username' => 'manager_tiga',
+        'nama' => 'Manager 3',
+        'password' => Hash::make('12345')
+    ];
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);    
+    UserModel::insert($data); // tambahkan data ke tabel m_user
+ 
+    // coba akses model UserModel
+    $user = UserModel::all(); // ambil semua data dari tabel m_user
+    return view('user', ['data' => $user]);
     }
 }
