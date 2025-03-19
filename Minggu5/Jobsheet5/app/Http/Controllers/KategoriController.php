b3827ef3a9f776e0417511dc3f2c585ad02365e5
dahniardd <dahniardvn09@gmail.com>
2025-03-19 09:35:18 +0700

    add praktikum 3

--- Minggu5/Jobsheet5/app/Http/Controllers/KategoriController.php
+++ Minggu5/Jobsheet5/app/Http/Controllers/KategoriController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\KategoriModel;
 use App\DataTables\KategoriDataTable;
 
 class KategoriController extends Controller
@@ -10,5 +11,17 @@ class KategoriController extends Controller
     public function index(KategoriDataTable $dataTable)
     {
         return $dataTable->render('kategori.index');
+    }
+    public function create()
+    {
+        return view('kategori.create');
+    }
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
     }
 }
