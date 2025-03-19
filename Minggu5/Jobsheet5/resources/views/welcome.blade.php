f7a53cd8a71f2c9efc0c56b3990473862af92b77
dahniardd <dahniardvn09@gmail.com>
2025-03-19 10:00:59 +0700

    add tugas 2 > menu manage kategori pada navbar

--- Minggu5/Jobsheet5/resources/views/welcome.blade.php
+++ Minggu5/Jobsheet5/resources/views/welcome.blade.php
@@ -2,25 +2,23 @@
 
 {{-- Customize layout sections --}}
 
-@section('subtitle', 'Welcome')
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Welcome')
+@section('title', 'Welcome') {{-- Menghindari subtitle yang duplikat --}}
+@section('content_header')
+    <h1>Welcome</h1>
+@endsection
 
 {{-- Content body: main page content --}}
-
-@section('content_body')
+@section('content')
     <p>Welcome to this beautiful admin panel.</p>
-@stop
+@endsection
 
 {{-- Push extra CSS --}}
-
 @push('css')
     {{-- Add here extra stylesheets --}}
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @endpush
 
 {{-- Push extra scripts --}}
-
 @push('js')
     <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
 @endpush
