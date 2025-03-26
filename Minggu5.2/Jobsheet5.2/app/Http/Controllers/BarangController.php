f2c0b592c71ab95d57cdcf9637c6a62613c448d7
dahniardd <dahniardvn09@gmail.com>
2025-03-26 21:40:42 +0700

    modifikasi tabel m_barang

--- Minggu5.2/Jobsheet5.2/app/Http/Controllers/BarangController.php
+++ Minggu5.2/Jobsheet5.2/app/Http/Controllers/BarangController.php
@@ -40,7 +40,7 @@ class BarangController extends Controller
              ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
              ->addColumn('aksi', function ($barang) { // menambahkan kolom aksi
                  $btn = '<a href="' . url('/barang/' . $barang->barang_id) . '" class="btn btn-info btn-sm">Detail</a> ';
-                 $btn .= '<a href="' . url('/barang/' . $barang->barang_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                 $btn .= '<a href="' . url('/barang/' . $barang->barang_id . '/edit') . '" class="btn btn-success btn-sm">Edit</a> ';
                  $btn .= '<form class="d-inline-block" method="POST" action="' . url('/barang/' . $barang->barang_id) . '">'
                      . csrf_field() . method_field('DELETE') .
                      '<button type="submit" class="btn btn-danger btn-sm"
