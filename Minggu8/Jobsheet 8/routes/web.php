b26def35fa56fb1664a70e8e5fd4af0692d33089
dahniardd <dahniardvn09@gmail.com>
2025-04-15 19:20:48 +0700

    add tugas 2 > export excel data kategori

--- Minggu8/Jobsheet 8/routes/web.php
+++ Minggu8/Jobsheet 8/routes/web.php
@@ -101,6 +101,7 @@ Route::middleware(['auth'])->group(function () {
             // Import Kategori with Excel
             Route::get('import', [KategoriController::class, 'import']); // ajax form upload excel
             Route::post('import_ajax', [KategoriController::class, 'import_ajax']); // ajax import excel
+            Route::get('export_excel', [KategoriController::class, 'export_excel']); //export excel
         });
     });
 
