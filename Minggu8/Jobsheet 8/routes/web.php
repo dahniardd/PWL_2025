858b4196531ef4c237dc6b5225380f56a4554181
dahniardd <dahniardvn09@gmail.com>
2025-04-15 19:00:07 +0700

    add praktikum 2 > export data barang

--- Minggu8/Jobsheet 8/routes/web.php
+++ Minggu8/Jobsheet 8/routes/web.php
@@ -132,6 +132,7 @@ Route::middleware(['auth'])->group(function () {
             // Import Barang with Excel
             Route::get('import', [BarangController::class, 'import']); // ajax form upload excel
             Route::post('import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+            Route::get('export_excel', [BarangController::class, 'export_excel']); // Export Barang excel
 
         });
     });
