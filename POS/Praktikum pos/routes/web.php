47a17e4180063b18280e99aef406bc68ba0c6b31
dahniardd <dahniardvn09@gmail.com>
2025-04-16 11:47:19 +0700

    add implementasi js8 > export pdf data barang

--- POS/Praktikum pos/routes/web.php
+++ POS/Praktikum pos/routes/web.php
@@ -144,7 +144,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('import', [BarangController::class, 'import']); // ajax form upload excel
             Route::post('import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
             Route::get('export_excel', [BarangController::class, 'export_excel']); // Export Barang excel
-            
+            Route::get('export_pdf', [BarangController::class, 'export_pdf']); // Export Barang pdf
         });
     });
 
@@ -173,7 +173,8 @@ Route::middleware(['auth'])->group(function () {
             Route::get('import', [SupplierController::class, 'import']); // ajax form upload excel
             Route::post('import_ajax', [SupplierController::class, 'import_ajax']); // ajax import excel
             Route::get('export_excel', [SupplierController::class, 'export_excel']); //export excel
-            
+             // Export Supplier with Pdf
+             Route::get('export_pdf', [SupplierController::class, 'export_pdf']); //export pdf
         });
     });
 });
