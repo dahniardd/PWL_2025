73db54d5cc09c434ca5c13c5e9ddc085625d19b1
dahniardd <dahniardvn09@gmail.com>
2025-04-30 08:49:43 +0700

    add Jobsheet 11

---
+++ Minggu11/Jobsheet11/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.template')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">🔥 Welcome back, Captain. Let’s get things done!</h3>
+        <div class="card-tools"></div>
+    </div>
+    <div class="card-body">
+    Data’s fresh, sistem on fire, tinggal kamu yang action.  
+    Mau input, update, atau ngatur strategi — <br> Semesta dashboard siap nemenin kamu!🚀
+    </div>
+</div>
+@endsection
